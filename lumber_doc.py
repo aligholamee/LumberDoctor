c3df4d841199ae44c69e77f60f3956b0079e06b4
Ali Gholami <aghce@aut.ac.ir>
2017-04-08 17:38:34 +0330

    use wx library for taking shots

--- lumber_doc.py
+++ lumber_doc.py
@@ -9,7 +9,13 @@
 # ========================================
 #
 
-import os
-import pygame
+import wx
 
 
+
+# Take an screenshot of the current page
+surface = pygame.display.set_mode((100, 100), 0, 32)
+surface.fill((255, 255, 255))
+pygame.draw.circle(surface, (0, 0, 0), (10, 10), 15, 0)
+pygame.display.update()
+pygame.image.save(surface,os.path.expanduser("pic1.png"))
