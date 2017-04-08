c30119efc388df037dca50fc1ea2809858c81b55
Ali Gholami <aghce@aut.ac.ir>
2017-04-08 17:46:08 +0330

    Logic was completed

--- lumber_doc.py
+++ lumber_doc.py
@@ -19,7 +19,9 @@
 # Right_coord = 712 * 450
 # Left_coord = 831 * 450
 
-
+# This is the placement of Mr lumber jack
+# Right_placement = 834 * 525
+# Left_placement = 715 * 525
 
 import cv2
 import play_game
@@ -28,17 +30,28 @@ import pyautogui
 
 has_won = False
 
+
 while ~has_won:
     image = pyautogui.screenshot()
     right_temp_pixel = image.getpixel(712, 450)
     left_temp_pixel = image.getpixel(831,450)
-    if right_temp_pixel == (211,247,255):
-        # Right is ok! click right!
-        play_game.click("right")
-    elif right_temp_pixel == (211,247,255):
-        # Right is a tree, click left
-        play_game.click("left")
-    elif left_temp_pixel
+    right_place = image.getpixel(834,525)
+    left_place = image.getpixel(715,525)
+    if right_place == (207,70,59):
+        if right_temp_pixel == (211,247,255):
+            # Right is ok! click right!
+            play_game.click("right")
+        elif right_temp_pixel == (211,247,255):
+            # Right is a tree, click left
+            play_game.click("left")
+
+    elif left_place == (207,70,59):
+        if left_temp_pixel == (211,247,255):
+            # left is ok! click left!
+            play_game.click("left")
+        elif left_temp_pixel == (211,247,255):
+            # left is a tree, click right
+            play_game.click("right")
 
 
 
