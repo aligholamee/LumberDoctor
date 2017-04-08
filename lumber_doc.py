141c97b879720240f4cb94e9879dca8bc93826ca
Ali Gholami <aghce@aut.ac.ir>
2017-04-08 15:42:02 +0330

    Screenshot works now - App object had to be declared

--- lumber_doc.py
+++ lumber_doc.py
@@ -10,13 +10,11 @@
 #
 
 import wx
-
-# Take an screenshot of the current page
-wx.App()    # Need to create an App instance before doing anything
+app = wx.App()  # Need to create an App instance before doing anything
 screen = wx.ScreenDC()
-size = screen.getSize()
-bmp = wx.emptyBitmap(size[0],size[1])
+size = screen.GetSize()
+bmp = wx.EmptyBitmap(size[0], size[1])
 mem = wx.MemoryDC(bmp)
-mem.Blit(0,0,size[0],size[1],screen,0,0)
-del mem     # Release bitmap
+mem.Blit(0, 0, size[0], size[1], screen, 0, 0)
+del mem  # Release bitmap
 bmp.SaveFile('screenshot.png', wx.BITMAP_TYPE_PNG)
