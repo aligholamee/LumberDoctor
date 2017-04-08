c47a282fdf7c7c498296ddce33f38fa5c76de245
Ali Gholami <aghce@aut.ac.ir>
2017-04-08 17:51:16 +0330

    Save the shot in a bmp file

--- lumber_doc.py
+++ lumber_doc.py
@@ -18,3 +18,5 @@ size = screen.getSize()
 bmp = wx.emptyBitmap(size[0],size[1])
 mem = wx.MemoryDC(bmp)
 mem.Blit(0,0,size[0],size[1],screen,0,0)
+del mem     # Release bitmap
+bmp.SaveFile('screenshot.png', wx.BITMAP_TYPE_PNG)
